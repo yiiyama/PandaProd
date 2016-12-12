81af276673360a284e73448cabbe62bb0fd752ad
Yutaro Iiyama <yiiyama@mit.edu>
2016-12-12 09:27:51 -0500

    branch list cleaned

---
+++ Producer/python/utils/puppi_cff.py
@@ -0,0 +1,42 @@
+import FWCore.ParameterSet.Config as cms
+
+# Copied from PhysicsTools.PatAlgos.slimming.puppiForMET_cff import makePuppiesFromMiniAOD
+from CommonTools.PileupAlgos.Puppi_cff import puppi
+puppi.candName = 'packedPFCandidates'
+puppi.vertexName = 'offlineSlimmedPrimaryVertices'
+puppi.useExistingWeights = False # I still don't trust miniaod...
+puppi.clonePackedCands = True # if !useExistingWeights, need to set this flag to make PuppiProducer create packed candidates
+
+pfNoLepPUPPI = cms.EDFilter("CandPtrSelector",
+    src = cms.InputTag("packedPFCandidates"),
+    cut = cms.string("abs(pdgId) != 13 && abs(pdgId) != 11 && abs(pdgId) != 15")
+)
+puppiNoLep = puppi.clone(
+    candName = 'pfNoLepPUPPI',
+    useWeightsNoLep = True
+)
+
+pfLeptonsPUPPET = cms.EDFilter("CandPtrSelector",
+    src = cms.InputTag("packedPFCandidates"),
+    cut = cms.string("abs(pdgId) == 13 || abs(pdgId) == 11 || abs(pdgId) == 15")
+)
+ 
+puppiMerged = cms.EDProducer("CandViewMerger",
+    src = cms.VInputTag('puppiNoLep', 'pfLeptonsPUPPET')
+)
+
+from CommonTools.PileupAlgos.PhotonPuppi_cff import puppiPhoton
+puppiForMET = puppiPhoton.clone(
+    puppiCandName = 'puppiMerged',
+    useRefs = False, # need to perform dR matching because of "an issue in refs in PackedCandidates"
+    photonId  = 'egmPhotonIDs:cutBasedPhotonID-Spring15-25ns-V1-standalone-loose' # match what's used for the actual photon collection
+)
+
+puppiSequence = cms.Sequence(
+    puppi +
+    pfNoLepPUPPI +
+    pfLeptonsPUPPET +
+    puppiNoLep +
+    puppiMerged +
+    puppiForMET
+)

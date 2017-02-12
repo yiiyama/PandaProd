1c5153d972a5510752c181ed0acb9059fc497df5
Yutaro Iiyama <yiiyama@mit.edu>
2017-02-11 19:00:48 -0500

    version for ReMINIAOD - need to add sequences back for MC

--- Producer/cfg/prod.py
+++ Producer/cfg/prod.py
@@ -47,13 +47,16 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 if options.globaltag == '':
     if options.isData:
         # sept reprocessing
-        process.GlobalTag.globaltag = '80X_dataRun2_2016SeptRepro_v3'
+        process.GlobalTag.globaltag = '80X_dataRun2_2016SeptRepro_v7'
     else:
-        ## tranche IV v6 ... is this correct?
-        process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_miniAODv2' # for 8011 MC? 
-        # process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_TrancheIV_v6'
+        process.GlobalTag.globaltag = '80X_mcRun2_asymptotic_2016_TrancheIV_v8'
 else:
     process.GlobalTag.globaltag = options.globaltag
+
+process.RandomNumberGeneratorService.panda = cms.PSet(
+    initialSeed = cms.untracked.uint32(1234567),
+    engineName = cms.untracked.string('TRandom3')
+)
 
 #############################
 ## RECO SEQUENCE AND SKIMS ##
@@ -73,6 +76,7 @@ process.MonoXFilter.taggingMode = True
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import setupAllVIDIdsInModule, setupVIDElectronSelection, setupVIDPhotonSelection, switchOnVIDPhotonIdProducer, switchOnVIDElectronIdProducer, DataFormat
 switchOnVIDPhotonIdProducer(process, DataFormat.MiniAOD)
 switchOnVIDElectronIdProducer(process, DataFormat.MiniAOD)
+setupAllVIDIdsInModule(process, 'RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring15_25ns_V1_cff', setupVIDPhotonSelection)
 setupAllVIDIdsInModule(process, 'RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring16_V2p2_cff', setupVIDPhotonSelection)
 setupAllVIDIdsInModule(process, 'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Summer16_80X_V1_cff', setupVIDElectronSelection)
 setupAllVIDIdsInModule(process, 'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronHLTPreselecition_Summer16_V1_cff', setupVIDElectronSelection)
@@ -87,86 +91,72 @@ egmIdSequence = cms.Sequence(
 )
 
 ### PUPPI
+
 process.load('PandaProd.Producer.utils.puppi_cff')
 from PandaProd.Producer.utils.puppi_cff import puppiSequence
 
-### RECLUSTER PUPPI JET
-from PandaProd.Producer.utils.makeJets_cff import makeJets
-puppiJetSequence = makeJets(process, options.isData, 'AK4PFPuppi', 'puppiForMET', 'Puppi')
-
-### JET RE-CORRECTION, TAGGING, AND SLIMMING
-from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJetCorrFactors, updatedPatJets
-
-process.updatedPatJetCorrFactors = updatedPatJetCorrFactors.clone(
-    src = cms.InputTag('slimmedJets', '', cms.InputTag.skipCurrentProcess()),
-    levels = cms.vstring(*jecLevels),
-)
+### QG TAGGING
 
 from RecoJets.JetProducers.QGTagger_cfi import QGTagger
-
-process.slimmedJets = updatedPatJets.clone(
-    jetSource = cms.InputTag('slimmedJets', '', cms.InputTag.skipCurrentProcess()),
-    addJetCorrFactors = cms.bool(True),
-    jetCorrFactorsSource = cms.VInputTag(cms.InputTag("updatedPatJetCorrFactors")),
-    addBTagInfo = cms.bool(False),
-    addDiscriminators = cms.bool(False)
-)
 
 process.QGTagger = QGTagger.clone(
     srcJets = 'slimmedJets',
     jetsLabel = cms.string('QGL_AK4PFchs')
 )
 
-jetRecorrectionSequence = cms.Sequence(
-    process.updatedPatJetCorrFactors +
-    process.slimmedJets +
+jetTaggingSequence = cms.Sequence(
     process.QGTagger
 )
 
 ### MET
-from PandaProd.Producer.utils.makeMET_cff import initMET, makeMET
 
-initMetSequence = initMET(process, options.isData)
-
-# extracts the raw pfMET from input MINIAOD and repack new corrections
-pfMetSequence = makeMET(process, options.isData, 'packedPFCandidates', 'slimmedJets', 'AK4PFchs')
-# compute a brand-new pfMET from puppi candidates and pack with corrections
-puppiMetSequence = makeMET(process, options.isData, 'puppiForMET', 'selectedJetsPuppi', 'AK4PFPuppi', 'Puppi')
-
-metSequence = cms.Sequence(
-    initMetSequence +
-    pfMetSequence +
-    puppiMetSequence
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+# process.fullPatMetSequence
+runMetCorAndUncFromMiniAOD(process, isData = options.isData)
+# process.fullPatMetSequencePuppi
+runMetCorAndUncFromMiniAOD(
+    process,
+    isData = options.isData,
+    metType = 'Puppi',
+    pfCandColl = 'puppiForMET',
+    recoMetFromPFCs = True,
+    jetFlavor = 'AK4PFPuppi',
+    postfix = 'Puppi'
 )
 
 ### FAT JETS
 
 from PandaProd.Producer.utils.makeFatJets_cff import initFatJets, makeFatJets
 
+# pfCHS set up here
 fatJetInitSequence = initFatJets(process, options.isData, ['AK8', 'CA15'])
 
 ak8CHSSequence = makeFatJets(
     process,
     isData = options.isData,
-    label = 'AK8PFchs'
+    label = 'AK8PFchs',
+    candidates = 'pfCHS'
 )
 
 ak8PuppiSequence = makeFatJets(
     process,
     isData = options.isData,
-    label = 'AK8PFPuppi'
+    label = 'AK8PFPuppi',
+    candidates = 'puppi'
 )
 
 ca15CHSSequence = makeFatJets(
     process,
     isData = options.isData,
-    label = 'CA15PFchs'
+    label = 'CA15PFchs',
+    candidates = 'pfCHS'
 )
 
 ca15PuppiSequence = makeFatJets(
     process,
     isData = options.isData,
-    label = 'CA15PFPuppi'
+    label = 'CA15PFPuppi',
+    candidates = 'puppi'
 )
 
 from PandaProd.Producer.utils.setupBTag import initBTag, setupDoubleBTag
@@ -188,36 +178,16 @@ fatJetSequence = cms.Sequence(
     ca15PuppiDoubleBTagSequence
 )
 
-### MET FILTERS
-process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
-process.BadPFMuonFilter.muons = cms.InputTag('slimmedMuons')
-process.BadPFMuonFilter.PFCandidates = cms.InputTag('packedPFCandidates')
-process.BadPFMuonFilter.taggingMode = cms.bool(True)
-
-process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
-process.BadChargedCandidateFilter.muons = cms.InputTag('slimmedMuons')
-process.BadChargedCandidateFilter.PFCandidates = cms.InputTag('packedPFCandidates')
-process.BadChargedCandidateFilter.taggingMode = cms.bool(True)
-
-metFilterSequence = cms.Sequence(
-    process.BadPFMuonFilter +
-    process.BadChargedCandidateFilter
-)
+### RECO PATH
 
 process.reco = cms.Path(
     process.MonoXFilter +
     egmIdSequence +
     puppiSequence +
-    puppiJetSequence +
-    jetRecorrectionSequence +
-    metSequence +
-    fatJetSequence +
-    metFilterSequence
-)
-
-process.RandomNumberGeneratorService.panda = cms.PSet(
-    initialSeed = cms.untracked.uint32(1234567),
-    engineName = cms.untracked.string('TRandom3')
+    jetTaggingSequence +
+#    process.fullPatMetSequence +
+#    process.fullPatMetSequencePuppi +
+    fatJetSequence
 )
 
 #############

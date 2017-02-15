ffb6e4cbb635581b5b477852734d1ee1017f566a
Yutaro Iiyama <yiiyama@mit.edu>
2017-02-15 02:04:39 -0500

    pulling my fix for PuppiPhoton

--- Producer/cfg/setuprel.sh
+++ Producer/cfg/setuprel.sh
@@ -4,5 +4,7 @@
 
 INSTALL=$CMSSW_BASE/src/PandaProd/Producer/scripts/install-pkg
 
-$INSTALL cms-met METRecipe_8020 RecoMET/METFilters CommonTools/PileupAlgos PhysicsTools/PatAlgos PhysicsTools/PatUtils RecoMET/METAlgorithms
+$INSTALL cms-met METRecipe_8020 RecoMET/METFilters PhysicsTools/PatAlgos PhysicsTools/PatUtils RecoMET/METAlgorithms #CommonTools/PileupAlgos
+# TEMPORARY UNTIL cms-met pulls this branch
+$INSTALL yiiyama puppiphoton_filteredpf CommonTools/PileupAlgos
 $INSTALL ikrav egm_id_80X_v3_photons PhysicsTools/SelectorUtils RecoEgamma/PhotonIdentification

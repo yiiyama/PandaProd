1fce780d45c91ed2865e4690d4ec728728174600
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-08 13:28:35 -0400

    bug fix genWeights

--- Producer/interface/WeightsFiller.h
+++ Producer/interface/WeightsFiller.h
@@ -40,12 +40,12 @@ class WeightsFiller : public FillerBase {
   static unsigned const learningPhase{100};
   
   std::vector<TString> wids_{};
-  float genParamBuffer_[learningPhase][1024]{};
+  float genParamBuffer_[learningPhase][panda::GenReweight::NMAX]{};
   unsigned bufferCounter_{0};
 
   double central_{0.};
   double normQCDVariations_[7]{}; // QCD variations (muR, muF, and PDF) normalized by originalXWGTUP
-  float genParam_[1024]{}; // I don't like that we hard-code the array size here..
+  float genParam_[panda::GenReweight::NMAX]{}; // I don't like that we hard-code the array size here..
 
   // these objects will be deleted automatically when the output file closes
   TH1D* hSumW_{0};

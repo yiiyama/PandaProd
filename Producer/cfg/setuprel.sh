c7f11dd8c2f33a44bdb6614e4c1dd8cc3e3b6cea
dabercro <daniel.r.abercrombie@gmail.com>
2017-11-07 10:55:50 -0500

    Should have defaults.

--- Producer/cfg/setuprel.sh
+++ Producer/cfg/setuprel.sh
@@ -4,6 +4,7 @@
 
 INSTALL=$CMSSW_BASE/src/PandaProd/Producer/scripts/install-pkg
 
+$INSTALL cms-met METRecipe_8020 PhysicsTools/PatAlgos PhysicsTools/PatUtils RecoMET/METAlgorithms #CommonTools/PileupAlgos
 # Loosen the dR matching between GS-fixed and nonfixed photons
 $INSTALL cms-met METRecipe_80X_part2 PhysicsTools/PatUtils RecoMET/METProducers
 # Check out the METFilters in the release and patch it
@@ -16,11 +17,9 @@ $INSTALL ikrav egm_id_80X_v3_photons PhysicsTools/SelectorUtils RecoEgamma/Photo
 
 # DeepCMVA and DeepCSV: https://twiki.cern.ch/twiki/bin/view/CMS/DeepFlavour
 # As you can see, the following line tries to overwrite PhysicsTools/PatAlgos...
-$INSTALL mverzett DeepFlavourCMVA-from-CMSSW_8_0_21 DataFormats/BTauReco PhysicsTools/PatAlgos RecoBTag/Configuration RecoBTag/DeepFlavour RecoBTag/LWTNN RecoBTag/SecondaryVertex
-mkdir $CMSSW_BASE/src/RecoBTag/DeepFlavour/data
-cd $CMSSW_BASE/src/RecoBTag/DeepFlavour/data
-wget http://home.fnal.gov/~verzetti//DeepFlavour/training/DeepFlavourNoSL.json
-wget http://mon.iihe.ac.be/~smoortga/DeepFlavour/CMSSW_implementation_DeepCMVA/Model_DeepCMVA.json
-cd -
-
-$INSTALL cms-met METRecipe_8020 PhysicsTools/PatAlgos PhysicsTools/PatUtils RecoMET/METAlgorithms #CommonTools/PileupAlgos
+#$INSTALL mverzett DeepFlavourCMVA-from-CMSSW_8_0_21 DataFormats/BTauReco RecoBTag/Configuration RecoBTag/DeepFlavour RecoBTag/LWTNN RecoBTag/SecondaryVertex # PhysicsTools/PatAlgos
+#mkdir $CMSSW_BASE/src/RecoBTag/DeepFlavour/data
+#cd $CMSSW_BASE/src/RecoBTag/DeepFlavour/data
+#wget http://home.fnal.gov/~verzetti//DeepFlavour/training/DeepFlavourNoSL.json
+#wget http://mon.iihe.ac.be/~smoortga/DeepFlavour/CMSSW_implementation_DeepCMVA/Model_DeepCMVA.json
+#cd -
